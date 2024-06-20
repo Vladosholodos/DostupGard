3201a83d66836833ec83ff6780304d974cf28a64
valdemarick <149163402+vamanadev@users.noreply.github.com>
2024-06-20 16:37:25 +0700

    Update Компиляция проекта.bat

--- Компиляция проекта.bat
+++ Компиляция проекта.bat
@@ -1,3 +1,3 @@
-cd C:\Users\vlado\Desktop\Пермяков практика языки прграмирования\ВКР\Проект\sketch_oct19a
+cd C:\Users\user\Desktop\sketch_oct19a
 
 pyinstaller --onefile --noconsole --add-data "Лого.png;." --add-data "config.ini;." lololo_v2.py
